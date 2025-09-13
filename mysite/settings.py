9ba2e2d190aeac85b2a293b712072b154ad7ebc8
tr3ndy661 <trendypotato661@gmail.com>
2025-09-13 18:35:06 +0300

    database created

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",  # Add this line
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# questions and published date
+class Question(models.Model):
+    qustion_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+# choices and votes
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
